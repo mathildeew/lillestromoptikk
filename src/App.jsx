1a119697a8573c6decdd221f22e0e470d1eb7b70
mathildeew <mathildewiik@hotmail.com>
2023-11-01 16:33:25 +0100

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
-export default function App() {
-  const [count, setCount] = useState(0);
+import { Routes, Route } from "react-router-dom";
 
-  return <></>;
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />}></Route>
+      </Route>
+    </Routes>
+  );
 }
