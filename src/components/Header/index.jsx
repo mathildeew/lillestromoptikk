0f8415ed2a3326ce4c2cb4a8fef1ba2a5f4e2e75
mathildeew <mathildewiik@hotmail.com>
2023-11-08 22:06:53 +0100

    header med nav

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,19 @@
 export default function Header() {
   return (
-    <header className="w-full border-black border-2">
-      <div className="flex justify-between items-center mx-6 my-3.5">
-        <img src="src/assets/logo.svg" className="w-40"></img>
-        <i class="fa-solid fa-bars fa-xl"></i>
-      </div>
-    </header>
+    <>
+      <header className="w-full border-black border-2">
+        <div className="flex justify-between items-center mx-6 my-3.5">
+          <img src="src/assets/logo.svg" className="w-40"></img>
+          <i class="fa-solid fa-bars fa-xl"></i>
+        </div>
+      </header>
+
+      <nav className="flex flex-col items-end text-end py-16">
+        <ul>
+          <li className="mb-4">Synsndersøkelse</li>
+          <li className="mb-4">Kontakt oss</li>
+        </ul>
+      </nav>
+    </>
   );
 }
