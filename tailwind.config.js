aa2b96756b2ed98a8818b1978041df874b639b1e
Mathilde Wiik <mathilde@Mathildes-MBP.lan>
2024-01-08 14:50:41 +0100

    clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#ACC3FF",
         grey: "#EFEFEF",
         kleinBlue: "#1F1BBD",
         darkBlue: "#221896",
@@ -14,41 +13,14 @@ export default {
         sans: ["Open Sans, Arial, Tahoma, sans-serif"],
         heading: ["neue-haas-grotesk-display, sans-serif;"],
       },
-      fontSize: {
-        home: "10vh",
-      },
-      lineHeight: {
-        big: "40px",
-      },
 
       width: {
-        400: "450px",
         550: "550px",
         750: "750px",
         950: "950px",
         1100: "1100px",
       },
-      padding: {
-        c80: "20%",
-      },
-      backgroundImage: {
-        "hero-pattern": "url(/assets/elements/bg-hero.svg)",
-        "brille-bg": "url(/assets/elements/brille-blue-bg.svg)",
-        brilleslange: "url(/dist/assets/elements/brilleslange.svg)",
-        downBg: "url(/dist/assets/elements/downBg.svg)",
-        rotated: "url(/dist/assets/elements/rotatedBg.svg)",
-        moscot: "url(/dist/assets/elements/brands/moscot/COLLECTION_5.jpeg)",
-        balloonDog: "url(/dist/assets/elements/balloon-sale.jpg)",
-        sale: "url(/public/sale/sale-background.jpg)",
-      },
-      rotate: {
-        150: "150deg",
-        20: "20deg",
-      },
-      boxShadow: {
-        double:
-          "-2px -2px #90ABE8, -5px -5px #fff, 2px 2px #90ABE8, 5px 5px #fff",
-      },
+
       animation: {
         sale: "saleCTA 4s linear infinite",
         salePercent: "salePerc 4s linear infinite",
