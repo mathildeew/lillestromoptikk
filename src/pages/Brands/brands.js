109341df2c3e7d69c73c336322eae48920264b18
Mathilde Wiik <mathildewiik@hotmail.com>
2024-01-29 11:25:13 +0100

    adds masunaga

--- src/pages/Brands/brands.js
+++ src/pages/Brands/brands.js
@@ -17,31 +17,31 @@ export const brands = [
       "Hele kolleksjonen til Ørgreen er produsert i Japan, et land kjent for sin stolte tradisjon innen overlegen produksjon av briller og for å anvende den høyeste standarden på hvert trinn i prosessen. Å kombinere denne produksjonsetikken med den danske designkulturen for presisjon og ekspertise resulterer i et produkt som kjennetegnes av kjærlighet for detaljer og varer livet ut.",
     img: "/brands/orgreen/orgreen-optics-collection.jpg",
   },
-  {
-    id: "resrei",
-    name: "RES REI",
-    description1:
-      "Med en forkjærlighet for unikt og estetisk design har italienske RES REI et hovedfokus på detaljer rundt mønstre og farger. RES REI er skapt som et friskt og litt kantete alternativ til den klassike brilleindustrien, og er et godt valg for de som ønsker å skille seg ut.",
-    description2:
-      'Oliviero Zanon, designer og grunnlegger, insisterer alltid på å være tilstede når nye design, farger og materialer tar form, så han bor rett rundt hjørnet fra fabrikken - som en ekte designer. RES REI er latin og betyr "tingen", men det betyr også en idé med et bærekraftig konsept',
-    img: "/brands/resrei/SS2023_details_mod_Colosso-863-896_downsized.jpg",
-  },
+  // {
+  //   id: "resrei",
+  //   name: "RES REI",
+  //   description1:
+  //     "Med en forkjærlighet for unikt og estetisk design har italienske RES REI et hovedfokus på detaljer rundt mønstre og farger. RES REI er skapt som et friskt og litt kantete alternativ til den klassike brilleindustrien, og er et godt valg for de som ønsker å skille seg ut.",
+  //   description2:
+  //     'Oliviero Zanon, designer og grunnlegger, insisterer alltid på å være tilstede når nye design, farger og materialer tar form, så han bor rett rundt hjørnet fra fabrikken - som en ekte designer. RES REI er latin og betyr "tingen", men det betyr også en idé med et bærekraftig konsept',
+  //   img: "/brands/resrei/SS2023_details_mod_Colosso-863-896_downsized.jpg",
+  // },
   {
     id: "masunaga",
     name: "Masunaga",
     description1:
-      "Med en stolt historie som begynte i 1905, har Masunaga etablert seg som en fremtredende aktør i brilleverden. Grunnlagt av Gozaemon Masunaga i Fukui, Japan, var selskapet dedikert til å plassere den optiske industrien solid i Fukui-regionen. Gjennom tidlige utfordringer med å overvinne oppfatningen av tredjerangs kvalitet, tok Masunaga på seg oppgaven med å forbedre brilleproduksjonen i området.",
+      "Med en stolt historie som begynte i 1905, har Masunaga etablert seg som en fremtredende aktør i brilleverden. Masunaga skiller seg ut ved å være det eneste selskapet i Japan med en integrert produksjonsprosess, håndtert internt fra råvarer til ferdigstillelse. Denne unike tilnærmingen sikrer en høy standard for håndverk og detaljer i hvert trinn av produksjonen.",
     description2:
-      "Masunaga skiller seg ut ved å være det eneste selskapet i Japan med en integrert produksjonsprosess, håndtert internt fra råvarer til ferdigstillelse. Dette unike tilnærmingen sikrer en høy standard for håndverk og detaljer i hvert trinn av produksjonen. Med fokus på innovasjon og kvalitet har Masunaga  etablert seg som en pålitelig leder innen brilleindustrien, og  engasjementet for håndtverk og eksklusivitet har gjort Masunaga til et ettertraktet navn blant de som verdsetter både stil og substans.",
-    img: "/brands/masunaga/MARGOT_1 downsized.jpeg",
+      "Med fokus på innovasjon og kvalitet har Masunaga  etablert seg som en pålitelig leder innen brilleindustrien, og  engasjementet for håndtverk og eksklusivitet har gjort Masunaga til et ettertraktet navn blant de som verdsetter både stil og substans.",
+    img: "/brands/masunaga/GMS123_124_2.jpg",
   },
-  {
-    id: "gotti",
-    name: "Götti",
-    description1:
-      "Mannen bak er designer og optiker Sven Götti. Han er skikkelig fan av minimalistisk brilledesign, der brillene hans bare har det som trengs. Götti Eyewear er kjent for å være ordentlig godt laget, og designet er enkelt og tidløst. Det går aldri av moten. Det er ekte sveitsisk stil med fokus på detaljer og håndverk. Nylig har Götti Eyewear slengt ut to nye kule kolleksjoner.",
-    description2:
-      "Götti Dimension er laget med 3D-magi og inspirert av byliv, kunst, arkitektur, mote og naturens råskap. Götti Perspective er som en superperfeksjonert nyvinning av briller uten ramme. Stenger, hengsler og sidebiter er satt sammen uten skruer, lim eller lodding. Begge kolleksjonene lages på Göttis fabrikk i Sveits.",
-    img: "/brands/masunaga/MARGOT_1 downsized.jpeg",
-  },
+  // {
+  //   id: "gotti",
+  //   name: "Götti",
+  //   description1:
+  //     "Mannen bak er designer og optiker Sven Götti. Han er skikkelig fan av minimalistisk brilledesign, der brillene hans bare har det som trengs. Götti Eyewear er kjent for å være ordentlig godt laget, og designet er enkelt og tidløst. Det går aldri av moten. Det er ekte sveitsisk stil med fokus på detaljer og håndverk. Nylig har Götti Eyewear slengt ut to nye kule kolleksjoner.",
+  //   description2:
+  //     "Götti Dimension er laget med 3D-magi og inspirert av byliv, kunst, arkitektur, mote og naturens råskap. Götti Perspective er som en superperfeksjonert nyvinning av briller uten ramme. Stenger, hengsler og sidebiter er satt sammen uten skruer, lim eller lodding. Begge kolleksjonene lages på Göttis fabrikk i Sveits.",
+  //   img: "/brands/masunaga/MARGOT_1 downsized.jpeg",
+  // },
 ];
--- src/pages/Brands/index.jsx
+++ src/pages/Brands/index.jsx
@@ -19,14 +19,14 @@ export default function Brands() {
             <section
               key={brand.id}
               className={`flex flex-col gap-6 lg:flex-row lg:items-center ${
-                brand.id === "moscot" || brand.id === "resrei"
+                brand.id === "moscot" || brand.id === "masunaga"
                   ? "flex-row"
                   : "lg:flex-row-reverse"
               }`}
             >
               <img
                 src={brand.img}
-                className="w-full h-44 object-cover object-center lg:w-96 lg:h-96"
+                className="w-full h-44 object-cover object-center md:h-72 lg:w-96 lg:h-96"
                 alt={`Vi har briller fra ${brand.name}`}
               />
               <div className="flex flex-col gap-4">
@@ -36,55 +36,7 @@ export default function Brands() {
               </div>
             </section>
           ))}
-          {/* <section className="flex flex-col gap-6 lg:flex-row lg:items-center">
-            <img
-              src="/brands/moscot/moscot-collection.jpg"
-              className="w-full h-44 object-cover object-center lg:w-96 lg:h-96"
-              alt="Vi har briller fra Moscot"
-            />
-            <div className="flex flex-col gap-4">
-              <h2>Moscot</h2>
-              <p>
-                MOSCOT har i over 100 år vært tro mot sine opprinnelige design
-                som går tilbake til 1920-tallet. Selskapets grunnlegger, Hyman
-                Moscot, immigrerte til USA fra Øst-Europa tidlig på 1900-tallet.
-                Siden den gang har selskapet vært drevet av Moscot-familien og
-                ledes nå av 4. og 5. generasjon, Dr. Harvey Moscot og sønnen
-                hans, Zachary Moscot.
-              </p>
-              <p>
-                MOSCOT består av klassiske retrobriller i ekte New York-stil, og
-                designet holder fortsatt i dag. Kolleksjonene er unike og
-                autentiske med materialer av titan, stål og acetat.
-              </p>
-            </div>
-          </section>
 
-          <section className="flex flex-col gap-6 lg:flex-row-reverse lg:items-center">
-            <img
-              src="/brands/orgreen/orgreen-optics-collection.jpg"
-              className="w-full h-44 object-cover object-center lg:w-96 lg:h-96"
-              alt="Vi har briller fra Ørgreen"
-            />
-            <div className="flex flex-col gap-4">
-              <h2>Ørgreen</h2>
-              <p>
-                Ørgreen er et internasjonalt merke fra København, som lager
-                førsteklasses briller i materialer av høy kvalitet. Kjent for
-                sine dynamiske design og tekniske presisjon, produserer Ørgreen
-                håndlagde rammer med unike fargekombinasjoner.
-              </p>
-              <p>
-                Hele kolleksjonen til Ørgreen er produsert i Japan, et land
-                kjent for sin stolte tradisjon innen overlegen produksjon av
-                briller og for å anvende den høyeste standarden på hvert trinn i
-                prosessen. Å kombinere denne produksjonsetikken med den danske
-                designkulturen for presisjon og ekspertise resulterer i et
-                produkt som kjennetegnes av kjærlighet for detaljer og varer
-                livet ut.
-              </p>
-            </div>
-          </section> */}
           <p className="font-semibold">
             Vi fører også merker som Tom Ford, Ray-Ban, Face a Face, Fleye,
             Traction Production og mange fler.
