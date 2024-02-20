1ec874643470f7af7e2efe216aa7138d1cb8c76e
Mathilde Wiik <mathildewiik@hotmail.com>
2024-02-20 16:45:34 +0100

    new layout

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,27 +1,25 @@
+import SlidingGlasses from "../../SlidingGlasses";
 import ScrollingGlasses from "../ScrollingGlasses";
 
 export default function Hero() {
   return (
     <div className="text-white bg-kleinBlue h-full flex flex-col items-center justify-center">
-      <div className="text-black uppercase tracking-wider bg-white w-full px-4 py-32 lg:hidden">
-        <h1 className=" z-20 relative">Din lokale optiker</h1>
-        <ScrollingGlasses />
-      </div>
-      <div className="max-w-3xl flex flex-col gap-8 px-4 py-20 lg:px-8">
-        <h1 className="hidden lg:inline uppercase tracking-wider">
-          Din lokale optiker
-        </h1>
-        <p className="z-20">
+      {/* <ScrollingGlasses /> */}
+      <SlidingGlasses />
+
+      <div className="max-w-3xl flex flex-col gap-8 px-4 py-20 z-20 lg:px-8">
+        <h2>Vi er din lokale optiker</h2>
+        <p>
           Med flere års erfaring og stort engasjement for å gi et klart,
           klarere, klarest syn tilbyr vi grundige synsundersøkelser med topp
           moderne utstyr.
         </p>
-        <p className="z-20">
+        <p>
           Hos oss finner du også et nøye kuratert utvalg av innfatninger og
           solbriller fra våre favorittmerker — alltid med kvalitet,
           individualitet og varighet i tankene.
         </p>
-        <p className="z-20"> Og psst, kaffen står alltid klar!</p>
+        <p> Og psst, kaffen står alltid klar!</p>
       </div>
     </div>
   );
