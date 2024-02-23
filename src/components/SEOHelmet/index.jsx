3079a5a5e32c9e055af6063886a22a57a53bbb90
Mathilde Wiik <mathildewiik@hotmail.com>
2024-02-23 10:59:47 +0100

    updates favicon for all devices

--- src/components/SEOHelmet/index.jsx
+++ src/components/SEOHelmet/index.jsx
@@ -9,7 +9,32 @@ export default function SEOHelmet({ title, content }) {
           src="https://kit.fontawesome.com/831e6c6172.js"
           crossorigin="anonymous"
         ></script>
-        <link rel="icon" type="image/x-icon" href="/identity/favicon.svg" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="identity/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="identity/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="identity/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/identity/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/identity/favicon/safari-pinned-tab.svg"
+          color="#1f1bbd"
+        />
+        <meta name="msapplication-TileColor" content="#1f1bbd" />
+        <meta name="theme-color" content="#ffffff" />
+
         <meta name="description" content={content} />
         <title>Lillestrøm Optikk | {title}</title>
       </Helmet>
