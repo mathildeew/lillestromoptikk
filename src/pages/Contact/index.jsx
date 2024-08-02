52ac623e63e2fea3237e2c7eb95eb922afecb285
Mathilde Wiik <mathildewiik@hotmail.com>
2024-08-02 15:57:40 +0200

    fixed typo

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -14,7 +14,7 @@ export default function Contact() {
           <section className="w-full text-center bg-grey flex flex-col items-center gap-6 px-4 py-10 md:px-10 lg:py-20">
             <div className="text-center max-w-4xl flex flex-col gap-4">
               <h1>Kontakt oss</h1>
-              <p>Send oss en e-post, så tar vi konatakt så fort vi kan. Du kan også ringe oss, så får du kanskje svar på det du lurer på med en gang.</p>
+              <p>Send oss en e-post, så tar vi kontakt så fort vi kan. Du kan også ringe oss, så får du kanskje svar på det du lurer på med en gang.</p>
             </div>
 
             <div className="flex flex-wrap gap-3 justify-center">
