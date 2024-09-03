88d4f05f973922635055969ce2c31957e391ff9c
Mathilde Wiik <mathildewiik@hotmail.com>
2024-09-03 22:52:38 +0200

    adds new styles

--- lo-sanity-studio/schemaTypes/home/hero.js
+++ lo-sanity-studio/schemaTypes/home/hero.js
@@ -9,11 +9,45 @@ const hero = {
       type: 'string',
     },
     {
-      name: 'images',
-      title: 'Bilder til karusell',
-      type: 'array',
-      of: [{type: 'imageSection'}],
+      name: 'image',
+      title: 'Bilde',
+      type: 'image',
+      description: 'Bør være under 200KB.',
+      validation: (rule) => rule.required(),
+      options: {hotspot: true},
+      fields: [
+        {
+          name: 'alt',
+          title: 'Alternativ tekst',
+          type: 'string',
+          description: 'Enkel beskrivelse av bildet, 5-10 ord.',
+          validation: (rule) => rule.required(),
+        },
+      ],
     },
+    {
+      name: 'content',
+      title: 'Tekst',
+      type: 'array',
+      description: "Vises ved siden av bildet på forsiden.",
+      validation: (rule) => rule.required(),
+      of: [
+        {
+          type: 'block',
+          styles: [
+            {
+              value: 'normal',
+            },
+          ],
+        },
+      ],
+    },
+    // {
+    //   name: 'images',
+    //   title: 'Bilder til karusell',
+    //   type: 'array',
+    //   of: [{type: 'imageSection'}],
+    // },
   ],
 }
 
--- src/components/features/home/Hero.jsx
+++ src/components/features/home/Hero.jsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from "react";
-import { Fade } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
+import { PortableText } from "@portabletext/react";
 import useAPI from "../../../hooks/useAPI";
 import { apiQuieries } from "../../../sanity/apiQuieries";
-import { urlFor } from "../../../sanity/urlFor";
 
 export default function Hero() {
   const { fetchAPI } = useAPI();
-
   const [heroData, setHeroData] = useState([]);
 
   useEffect(() => {
@@ -18,27 +15,16 @@ export default function Hero() {
     getData();
   }, [fetchAPI]);
 
-  const properties = {
-    prevArrow: <button className="hidden"></button>,
-    nextArrow: <button className="hidden"></button>,
-  };
-
   return (
     <section className="w-full max-w-7xl h-fit px-4 pt-16 mx-auto md:px-10 lg:grid lg:grid-cols-2 lg:items-center lg:pt-20">
-      <div className="mr-10 lg:mr-0">
-        <Fade {...properties}>
-          {heroData?.images?.map((image) => (
-            <div key={image._key} style={{ backgroundImage: `url(${urlFor(image?.image).url()})` }} className="h-[420px] w-full bg-cover bg-center lg:h-[600px] rounded-xl"></div>
-          ))}
-        </Fade>
+      <div className="z-20 mr-6 lg:mr-0">
+        <img src={heroData.imageUrl} alt={heroData.imageAlt} className="w-full h-auto object-cover rounded-xl" />
       </div>
 
-      <div className="z-20 relative w-full">
-        <div className="bg-green361 flex flex-col items-start gap-3 border-2 border-black rounded-xl relative p-4 ml-10 -mt-10 md:p-12 md:gap-8 lg:-mt-0 lg:-ml-8">
-          <h2>Din lokale optiker i Lillestrøm</h2>
-          <p>Med flere års erfaring og stort engasjement for å gi et klart, klarere, klarest syn tilbyr vi grundige synsundersøkelser med topp moderne utstyr.</p>
-          <p>Hos oss finner du også et nøye kuratert utvalg av innfatninger og solbriller fra våre favorittmerker — alltid med kvalitet, individualitet og varighet i tankene.</p>
-          <p>Og psst, kaffen står alltid klar!</p>
+      <div className="w-full">
+        <div className="bg-grey flex flex-col items-start gap-3 border-2 border-black rounded-xl relative p-4 ml-6 -mt-4 md:p-12 md:gap-8 lg:-mt-0 lg:-ml-4">
+          <h2>{heroData.title}</h2>
+          <PortableText value={heroData.content} />
         </div>
       </div>
     </section>
