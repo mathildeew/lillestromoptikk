66a67afbd7b0024b35d6a60435eb86ef5d98e2a4
Mathilde Wiik <mathildewiik@hotmail.com>
2024-10-08 14:43:32 +0200

    adds animation

--- src/components/Layout/header/header.jsx
+++ src/components/Layout/header/header.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
 import DesktopNav from "./nav/desktop/DesktopNav";
 import MobileNav from "./nav/mobile/MobileNav";
 
