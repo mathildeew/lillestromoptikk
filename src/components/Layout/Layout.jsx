a4a249d073278760ac8dbcc3d4c5370e41858d11
Mathilde Wiik <mathildewiik@hotmail.com>
2024-10-11 15:09:02 +0200

    adds ending in import

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "./header/Header.jsx";
+import Footer from "./footer/Footer.jsx";
+
 export default function Layout() {
   return (
     <>
+      <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
