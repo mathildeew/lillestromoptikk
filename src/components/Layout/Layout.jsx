cb765b35581c87d00f576ecc2fe16568c0a12142
Mathilde Wiik <mathildewiik@hotmail.com>
2024-10-11 15:25:37 +0200

    testing

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "./header";
-import Footer from "./footer";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
 
 export default function Layout() {
   return (
--- src/components/Layout/header/header.jsx
+++ src/components/Layout/header/header.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import DesktopNav from "./nav/desktop/DesktopNav";
-import MobileNav from "./nav/mobile/MobileNav";
+// import DesktopNav from "./nav/desktop/DesktopNav";
+// import MobileNav from "./nav/mobile/MobileNav";
 
 export default function Header() {
   const [openMenu, setOpenMenu] = useState(false);
@@ -20,7 +20,7 @@ export default function Header() {
   return (
     <>
       <header className="bg-white w-full flex z-40 relative md:px-12 border-b-black border-b">
-        <div className="w-full max-w-7xl flex justify-between items-center px-6 md:mx-auto md:px-3.5">
+        {/* <div className="w-full max-w-7xl flex justify-between items-center px-6 md:mx-auto md:px-3.5">
           <Link to={`/`}>
             <img aria-label="Gå til forsiden" src="/identity/logo.svg" className="w-24 h-auto lg:w-36" alt="Lillestrøm Optikk logo" />
           </Link>
@@ -34,7 +34,7 @@ export default function Header() {
             <span className={`w-full h-0.5 bg-black absolute top-6 transition-right duration-500 ease-in-out ${openMenu && "-translate-x-4 opacity-0"}`}></span>
           </div>
         </div>
-        <MobileNav openMenu={openMenu} setOpenMenu={setOpenMenu} />
+        <MobileNav openMenu={openMenu} setOpenMenu={setOpenMenu} /> */}
       </header>
     </>
   );
