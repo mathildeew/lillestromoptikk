7fd5bb93a073bd4d8978a4a67f012a6bbaef9f57
Mathilde Wiik <mathildewiik@hotmail.com>
2024-10-11 15:34:29 +0200

    testing

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "src/components/layout/header/Header.jsx";
-import Footer from "src/components/layout/footer/Footer.jsx";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
 
 export default function Layout() {
   return (
